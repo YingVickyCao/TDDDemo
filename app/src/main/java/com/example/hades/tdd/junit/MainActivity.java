26eb5322dd097f40a9ce0cd14cbd6d3da84d83d6
Vicky <ying.vicky.cao@outlook.com>
2019-09-19 22:52:54 +0800

    Mrgrate TDD

---
+++ app/src/main/java/com/example/hades/tdd/junit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hades.tdd.junit;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hades.tdd.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
